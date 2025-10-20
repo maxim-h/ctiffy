bdd76d1c145eefd32107e558eada90ce7f288634
Maksim Kholmatov <maksim.holmatov@gmail.com>
2025-10-21 00:53:17 +0200

    Read seond byte pair. Figure out pointer arithmentic.
    Start defining structs

--- main.c
+++ main.c
@@ -10,9 +10,25 @@
 #include <fcntl.h>
 #include <stdint.h>
 
+typedef struct TIFF_HEADER {
+    enum  Byte_order {MM, II} byte_order;
+    uint64_t first_ifd_offset;
+
+} TIFF_HEADER;
+
+typedef struct IFD {
+    uint16_t n_entries;
+} IFD;
+
+uint16_t swap16(uint16_t x) {
+    return (x >> 8) | (x << 8);
+}
 
 int main() {
-    int fd = open("./data/tubhiswt_C0.ome.tif", O_RDONLY);
+
+
+    char input_file[] = "./data/tubhiswt_C0.ome.tif";
+    int fd = open(input_file, O_RDONLY);
     if (fd < 0) {
         perror("open");
         return 69;
@@ -23,23 +39,19 @@ int main() {
 
     size_t filesize = st.st_size;
 
-    uint32_t *map = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, fd, 0);
+    uint8_t *map = mmap(NULL, filesize, PROT_READ, MAP_PRIVATE, fd, 0);
     if (map == MAP_FAILED) {perror("mmap"); close(fd); return 69;}
 
-    off_t offset = 32;
 
-    if (offset + sizeof(uint32_t) > (off_t)filesize) {
-        fprintf(stderr, "offset out of range\n");
-        munmap(map, filesize);
-        close(fd);
-        return 1;
-    }
+    uint16_t byte_order = 0;
+    memcpy(&byte_order, map, 2);
 
+    printf("Byte order: %x\n", byte_order);
 
-    uint16_t bo = 0;
-    memcpy(&bo, map, 2);
+    uint16_t magic_number = 0;
+    memcpy(&magic_number, map+2, 2);
 
-    printf("%x\n", bo);
+    printf("Magic number: %d\n", (magic_number));
 
 
     munmap(map, filesize);
